c4c05362b77f8969333ba9513c6647c5179c81a3
Rômulo P. Costa <romulo@pinheirocosta.com>
2025-09-02 17:43:00 -0300

    refactor(parceria/admin): adiciona um novo mais amigavel para coluna periodicidade no painel admin

--- backend/parceria/models.py
+++ backend/parceria/models.py
@@ -12,7 +12,12 @@ class Servico(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
-    periodicidade_servico = models.CharField(max_length=10, choices=ServicoPeriodicidade.choices, default=ServicoPeriodicidade.AVULSO)
+    periodicidade_servico = models.CharField(
+        max_length=10,
+        choices=ServicoPeriodicidade.choices,
+        default=ServicoPeriodicidade.AVULSO,
+        verbose_name="Periodicidade"
+    )
 
     def __str__(self):
         return self.nome
